0694572f323134a1be60a4d88a8840a607d1e14d
Lucio Chen <luciochen233@gmail.com>
2020-01-21 15:18:46 -0500

    added ADC

---
+++ adc.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+constexpr int PIN_MAX = 80;
+constexpr char ADC_FOLDER[] = "/sys/class/gpio/gpio";
+
+class gpio {
+	int port;
+	int direction;
+	int value;
+public:
+	gpio(int);
+	int get_direction();
+	int get_value();
+	bool set_direction(int);
+	bool set_value(int);
+    ~gpio(){}
+};
