37380b142fc50b3128585b01963ef6c8986ad829
Lucio Chen <luciochen233@gmail.com>
2020-02-12 06:56:02 -0500

    improved reading float array

--- Experiment2/serial.cpp
+++ Experiment2/serial.cpp
@@ -1,71 +1,107 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
-#include <string>
 #include <iostream>
-#include <thread>
-#include <chrono>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-vector<char> decode(vector<char> buff) {
-	int position = 0;
-	int value = -1;
-	while(value != 0){
-		value = buff[position];
-		buff[position] = 0;
-		position += value;
-	}
-	return buff;
+class cobs_float_array {
+ private:
+  vector<char> buff;
+  vector<float> result;
+  int fd;
+  bool ava;
+  void read_stream();
+  void decode();
+  void translate();
+
+ public:
+  cobs_float_array();
+  ~cobs_float_array() { serialClose(fd); }
+  void read();
+
+  vector<float> getData() { return result; }
+  bool avaliable() { return ava; }
+
+  friend ostream &operator<<(ostream &os, const cobs_float_array &arr);
+};
+
+void cobs_float_array::read() {
+  read_stream();
+  decode();
+  translate();
 }
 
-vector<char> get_char_array(int fd){
-	vector<char> temp;
-	int size = 0;
-	while(serialDataAvail(fd)){
-		char te = serialGetchar(fd);
-		temp.push_back(te);
-		if(te == 0x00) return temp;
-		size++;
-		if(size > 100) return temp;
-	}
-	return temp;
+cobs_float_array::cobs_float_array() {
+  fd = serialOpen("/dev/serial0", 9600);
+  if (fd < 1) cout << "fd is worng " << fd << endl;
+  ava = false;
 }
 
-vector<float> char_to_float(vector<char> buff){
-	vector<float> temp;
-	char convert[4];
-	for(int i = 1; i< buff.size() - 1; i+=4){
-		convert[0] = buff[i];
-		convert[1] = buff[i+1];
-		convert[2] = buff[i+2];
-		convert[3] = buff[i+3];
-		temp.push_back((float)convert);
-	}
-	return temp;
+void cobs_float_array::read_stream() {
+  while (serialDataAvail(fd)) {
+    char te = serialGetchar(fd);
+    buff.push_back(te);
+    if (te == 0x00) {
+      ava = true;
+      return;
+    }
+    if (buff.size() > 256) {
+      buff.clear();
+      ava = false;  // this indicates a read data error
+    }
+  }
 }
 
-vector<float> do_everything(int fd){
-	vector<char> temp = get_char_array(fd);
-	temp = decode(temp);
-	return char_to_float(temp);
+void cobs_float_array::decode() {  // after decode, the first and last byte
+                                   // should be zero
+  if (!ava) return;
+  int position = 0;
+  int value = -1;
+  while (value != 0) {
+    value = buff[position];
+    buff[position] = 0;
+    position += value;
+    if (position > buff.size()) {
+      ava = false;  // this indicates a read data error
+      buff.clear();
+      return;
+    }
+  }
 }
 
-int main(){
-	//get_serial();
-	int fd = serialOpen("/dev/serial0", 9600);
-	if(fd <1) cout << "fd is worng " << fd << endl;
-	char temp = 0;
-	while(1){
-		if(serialDataAvail(fd)){
-			delay(400);
-			vector<float> l1 = do_everything(fd);
-			for(auto i: l1){
-				cout << i << " " << endl;
-			}
-		}
-	}
+void cobs_float_array::translate() {
+  if (!ava) return;
+  char convert[4];
+  for (int i = 1; i < buff.size() - 1; i += 4) {
+    convert[0] = buff[i];
+    convert[1] = buff[i + 1];
+    convert[2] = buff[i + 2];
+    convert[3] = buff[i + 3];
+    result.push_back(*(float *)convert);
+  }
+  buff.clear();
+}
 
-	cout << "This is the end" << endl;
-	return 0;
+ostream &operator<<(ostream &os, const vector<float> &f1) {
+  for (auto f : f1) {
+    os << f << ", ";
+  }
+  os << endl;
+  return os;
+}
+
+int main() {
+  cobs_float_array arr1;
+  while (1) {
+    arr1.read();
+    if (arr1.avaliable()) {
+      vector<float> temp = arr1.getData();
+      cout << temp << endl;
+    }
+  }
+
+  cout << "This is the end" << endl;
+  return 0;
 }
