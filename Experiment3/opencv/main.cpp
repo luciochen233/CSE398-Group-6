f28eff7b79214d3d996336e65bd7a60c3f8623cf
Guoliang Chen <luciochen233@gmail.com>
2020-02-24 13:01:27 -0500

    added Lab3 file

--- Experiment3/opencv/main.cpp
+++ Experiment3/opencv/main.cpp
@@ -2,11 +2,16 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 #include <iostream>
 #include <stdio.h>
+//#include <QtGui/QImage> //may be useful for displaying a live image
+
 using namespace cv;
 using namespace std;
 int main(int, char**)
+  
 {
     Mat frame;
     //--- INITIALIZE VIDEOCAPTURE
@@ -14,7 +19,7 @@ int main(int, char**)
     // open the default camera using default API
     // cap.open(0);
     // OR advance usage: select any API backend
-   
+    
     // open selected camera using selected API
     //cap.open(deviceID + apiID);
     // check if we succeeded
@@ -28,8 +33,14 @@ int main(int, char**)
     cap >> frame;
     // wait for a new frame from camera and store it into 'frame'
     // show live and wait for a key with timeout long enough to show images
-    imshow("Live", frame);
-     
+
+    while(true){ //hold down any key to display a live image
+      cap >> frame;
+      imshow("Live", frame);
+      waitKey(0);
+      //QImage disp = QImage((uchar*) frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
+    }
+   
     imwrite("capture.png",frame);
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
